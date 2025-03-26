a2500370534b3f9cbca9d1acd0cb242ffd642a7f
MohamedSaad00 <mohamed.saadx4@gmail.com>
2025-03-25 19:04:35 -0700

    Final update

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
 export PATH=$PATH:/usr/local/bin
 cd /app/backend
+
+echo "Starting Gunicorn server..."
 exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --timeout 120 "app:create_app()" 
